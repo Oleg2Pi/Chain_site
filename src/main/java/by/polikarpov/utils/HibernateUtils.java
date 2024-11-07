9104d92ba7da91aebb36366c097f94c9e344a85a
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-11-07 19:46:34 +0300

    Add postgres, lombok and hibernate into pom.xml Create hibernate.cfg.xml and hibernateUtils

---
+++ src/main/java/by/polikarpov/utils/HibernateUtils.java
@@ -0,0 +1,35 @@
+package by.polikarpov.utils;
+
+import by.polikarpov.entity.Person;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration config = new Configuration().configure();
+            config.addAnnotatedClass(Person.class);
+
+            sessionFactory = config.buildSessionFactory();
+        } catch (HibernateException e) {
+            System.err.println("Initial SessionFactory creation failed." + e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+    public static void closeSession(Session session) {
+        if (sessionFactory != null) {
+            session.close();
+        }
+    }
+
+}
